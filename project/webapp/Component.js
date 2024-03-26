96a5a738c6d469da8f73d6502a1e11e38f974888
Veranika Miadvedzeva <veranika.miadvedzeva@leverx.com>
2024-03-26 09:41:24 +0300

    set up fiori app

---
+++ project/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("veronchi.leverx.project.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
